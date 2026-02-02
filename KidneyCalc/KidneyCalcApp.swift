10bd51eb37c7181c85fe58f778983e99dd061584
James Kurian <jamesk543@icloud.com>
2026-02-02 09:34:08 -0600

    Initial Commit

---
+++ KidneyCalc/KidneyCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidneyCalcApp.swift
+//  KidneyCalc
+//
+//  Created by James Kurian on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct KidneyCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
